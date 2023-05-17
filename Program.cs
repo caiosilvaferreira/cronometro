60786b2a30a940e9d6767128296474a90dda882d
caiosilvaferreira <caiosilvaferreira1@outlook.com.br>
2023-05-17 20:55:19 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,66 @@
+﻿namespace cronometro {
+    internal class Program {
+        static void Main(string[] args) {
+
+            menu();
+        }
+
+
+        static void menu() { 
+            Console.Clear();
+            Console.WriteLine("S = Segundo => 10s = 10 segundos");
+            Console.WriteLine("M = Minuto => 1m = 1 minuto");
+            Console.WriteLine("0 = Sair");
+            Console.WriteLine("quanto tempo deseja contar? ");
+            string data = Console.ReadLine().ToLower(); // ToLower(); faz com que todos os caracteres de string sao convertidos para minusculo.
+
+            //char type = char.Parse(data.Substring(data.Length-1 aqui ele conta quantos caracteres o usuario digitou e com -1 ele pega o ultimo caractere digitado pelo usuario, 1 aqui ele pega somente um caractere)); 
+
+            //int time = int.Parse(data.Substring(0 ele vai pega toda informacao do inicio ate o fim determinado pelo desenvolvedor, data.Length-1 ele conta quantos caracteres o usuario digitou e exclui somente o ultimo caractere));
+
+            char type = char.Parse(data.Substring(data.Length-1,1));
+            int time = int.Parse(data.Substring(0, data.Length-1));
+            int multplier = 1;
+
+            if(type == 'm')
+                multplier = 60;
+
+            if (time == 0)
+                System.Environment.Exit(0);
+
+            PreStart(time * multplier);
+            
+        }
+
+        static void PreStart(int time) {
+
+            Console.Clear();
+            Console.WriteLine("preparar...");
+            Thread.Sleep(1000);
+            Console.WriteLine("apontar...");
+            Thread.Sleep(1000);
+            Console.WriteLine("fogo...");
+            Thread.Sleep(2500);
+
+            Start(time);
+
+        }
+        static void Start(int time) {
+
+            
+            int currimentTime = 0;
+            while (currimentTime!=time) {
+
+                Console.Clear();
+                currimentTime++;
+                Console.WriteLine(currimentTime);
+                Thread.Sleep(1000);
+            }
+
+            Console.Clear() ;
+            Console.WriteLine("Cronometro finalizado");
+            Thread.Sleep(2500);
+            menu();
+        }
+    }
+}
